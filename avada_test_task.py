2bad9fdf3d547ad251411eeca7bb29fef7e7e941
Shevtsov Sergey <shevadotka@gmail.com>
2023-03-22 14:46:09 +0200

    error

--- avada_test_task.py
+++ avada_test_task.py
@@ -4,9 +4,7 @@ from typing import List
 
 
 
-
-def get_schedule_generation(date_start: str, date_end: str, days_work: int, days_skip: int) -> List:
-
+def get_schedule_generation(date_start: str, date_end: str, days_work: int, days_skip: int) -> List[str]:
     """
     Функция генерации рабочих дней
 
@@ -15,27 +13,26 @@ def get_schedule_generation(date_start: str, date_end: str, days_work: int, days
     :param days_work:(int) - Сколько дней подряд парикмахер работает
     :param days_skip:(int) - Сколько дней после этого отдыхает
 
-    :return:(List)-Возвращать функция должна массив строк с датами в формате isoformat
+    :return:(List[str]) - Возвращать функция должна массив строк с датами в формате isoformat
     """
+    # Преобразуем аргументы в формат isoformat date
+    start_date = datetime.fromisoformat(date_start).date()
+    end_date = datetime.fromisoformat(date_end).date()
 
-    #Преобразуем аргументывформат isoformat date)
-    start_date =datetime.fromisoformat(date_start)
-    end_date = datetime.fromisoformat(date_end)
-
-    #Создаем пустой список для хранения рабочих дней
+    # Создаем пустой список для хранения рабочих дней
     list_work_days = []
 
-    #Создаем итератор с набором рабочих  и нерабочих дней
-    iter_work_and_skip_days = cycle(["work_days"]*days_work + ["skip_days"]*days_skip)
+    # Создаем итератор с набором рабочих и нерабочих дней
+    iter_work_and_skip_days = cycle(["work_days"] * days_work + ["skip_days"] * days_skip)
 
-    #Создаем цыкл который проходить с начала графика и до конца
+    # Продолжаем генерировать даты, пока не достигнем конца периода
     while start_date <= end_date:
+        # Если текущий день является рабочим, добавляем его в список рабочих дней
         if next(iter_work_and_skip_days) == "work_days":
-            list_work_days.append(date_start)
+            list_work_days.append(start_date.isoformat())
 
+        # Увеличиваем дату на один день
         start_date += timedelta(days=1)
-
-
 
     return list_work_days
 
@@ -44,3 +41,4 @@ if __name__ == "__main__":
     print(get_schedule_generation('2022-04-25', '2022-06-26', 0, 4))
     print(get_schedule_generation('2022-05-14', '2022-05-24', 1, 95))
     print(get_schedule_generation('2022-04-10', '2022-04-23', 1, 1))
+
